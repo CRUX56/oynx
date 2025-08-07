335aec3deff227097741da924c426cac8a83b370
CRUX56 <designers_crux@live.com>
2025-08-06 23:07:11 -0400

    CSS Updates 2
    - updates to the footer layout and css update.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,14 +1,34 @@
 import React from "react";
 import { Box, Typography, Grid } from "@mui/material";
-import ContactFormSection from "../ContactFormSection";
 
 export default function Footer({ logo, blurb, navigation, copyright }) {
   return (
-    <Box sx={{ backgroundColor: "#333", color: "white", pt: 4 }}>
+    <Box sx={{ backgroundColor: "#121212", color: "white", pt: 4 }}>
       <Grid container spacing={4} sx={{ px: 12 }}>
-        <Grid item xs={12} md={12} lg={12}>
+        <Grid item xs={12}>
           <Box sx={{ textAlign: "center", mb: 2 }}>
-            <span>Onxy®</span>
+            <span
+              style={{
+                fontSize: "260.41px",
+                fontWeight: "600",
+                textTransform: "Capitalize",
+                letterSpacing: "-0.08em",
+                lineHeight: "0.9em",
+                fontFamily: "Inter, sans-serif",
+              }}
+            >
+              {logo}
+            </span>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ textAlign: "center", mb: 2 }}>
+            <Typography variant="body1">{blurb}</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ textAlign: "center", mb: 2 }}>
+            <Typography variant="h6">Quick Links</Typography>
           </Box>
         </Grid>
       </Grid>
@@ -19,7 +39,7 @@ export default function Footer({ logo, blurb, navigation, copyright }) {
           textAlign: "center",
           py: 2,
           mt: 4,
-          backgroundColor: "#222",
+          backgroundColor: "#121212",
         }}
       >
         <Typography variant="body2">{copyright}</Typography>
